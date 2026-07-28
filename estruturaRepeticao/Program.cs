7c20b3ea6c2b9bbc2a53c66787cdecb7260069c6
RodrigoGondin <rodrigogondin.pro@gmail.com>
2026-07-27 21:37:32 -0300

    Second part of repetition structure lesson with while, do while and for

--- estruturaRepeticao/Program.cs
+++ estruturaRepeticao/Program.cs
@@ -39,12 +39,73 @@
 
 //Validacao completa de input
 
-Console.WriteLine ("Digite um numero");
-bool ehNumero = int.TryParse (Console.ReadLine(), out int num);
-while(!ehNumero )
-{
-    Console.WriteLine("Valor invalido. Digite um numero: ");
-    ehNumero = int.TryParse(Console.ReadLine(), out num);
-}
+// Console.WriteLine ("Digite um numero");
+// bool ehNumero = int.TryParse (Console.ReadLine(), out int num);
+// while(!ehNumero )
+// {
+//     Console.WriteLine("Valor invalido. Digite um numero: ");
+//     ehNumero = int.TryParse(Console.ReadLine(), out num);
+// }
 
-Console.WriteLine("O numero digitado foi "+ num);
+// Console.WriteLine("O numero digitado foi "+ num);
+
+
+//Tabuada utilizando a estrutura 'for'
+
+// for ( int tabuada =0; tabuada <= 20; tabuada++) 
+// {
+//     Console.WriteLine($"2 x {tabuada} = {tabuada *2}");
+// }
+
+//Calcule o fatorial de  N (obtido do usuario) e multiplicar todos os numeros antessesores positivos
+
+
+
+//receber o valor;
+
+Console.WriteLine("Digite um numero para calcular o fatorial: ");
+int fatorial = int.Parse(Console.ReadLine());
+
+
+//while 
+int fatorialWhile = fatorial;
+int resultadoWhile = 1;
+while (fatorialWhile > 1)
+{
+    resultadoWhile = resultadoWhile * fatorialWhile;
+    fatorialWhile--;
+}
+Console.WriteLine("resultado do fatorial de " + fatorial + "  é " + resultadoWhile);
+
+//do while 
+int doFatorial = fatorial;
+int doResultado = 1;
+
+do
+{
+    doResultado = doResultado * doFatorial;
+    doFatorial--;
+
+} while (doFatorial > 1);
+Console.WriteLine("resultado do fatorial de " + fatorial + "  é " + doResultado);
+
+
+//for 
+int forResultado = 1;
+for (int forFatorial = fatorial; forFatorial > 1; forFatorial--)
+{
+    forResultado = forResultado * forFatorial;
+}
+Console.WriteLine("resultado do fatorial de " + fatorial + "  é " + forResultado);
+
+
+//for 
+
+int resultado = 1;
+for (int i = 1; i <= fatorial; i++)
+{
+    resultado = resultado * i;
+}
+Console.WriteLine("resultado do fatorial de " + fatorial + "  é " + resultado);
+
+
